43ad9fa1a3fe2d8843e48708d46bb52fbb141856
Paul Grossetête <paulgrossetete.code@gmail.com>
2025-06-27 09:39:39 +0200

    Ajout des options de vérification des données ajoutées à la database

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/////////// COURSE EXERCICES ///////////
+
 require('dotenv').config()
 const express = require('express')
 const Note = require('./models/note')
@@ -9,89 +11,98 @@ app.use(express.json())
 let notes = []
 
 //#region ROUTES 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
 
-app.get('/api/notes', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
-})
-
-app.get('/api/notes/:id', (request, response, next) => {
-  Note.findById(request.params.id).then(note => {
-    if (note) {
-      response.json(note)
-    } else {
-      response.status(404).end()
-    }
-  })
-    .catch(error => next(error))
-})
-
-app.post('/api/notes', (request, response) => {
-  const body = request.body
-
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing',
+  //#region GET ROUTES
+    app.get('/', (request, response) => {
+      response.send('<h1>Hello World!</h1>')
     })
-  }
 
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-  })
-
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
-})
-
-app.delete('/api/notes/:id', (request, response, next) => {
-  Note.findByIdAndDelete(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
-})
-
-app.put('/api/notes/:id', (request, response, next) => {
-  const { content, important } = request.body
-
-  Note.findById(request.params.id)
-    .then(note => {
-      if (!note) {
-        return response.status(404).end()
-      }
-
-      note.content = content
-      note.important = important
-
-      return note.save().then((updatedNote) => {
-        response.json(updatedNote)
+    app.get('/api/notes', (request, response) => {
+      Note.find({}).then(notes => {
+        response.json(notes)
       })
     })
-    .catch((error) => {
-      return next(error)
+
+    app.get('/api/notes/:id', (request, response, next) => {
+      Note.findById(request.params.id).then(note => {
+        if (note) {
+          response.json(note)
+        } else {
+          response.status(404).end()
+        }
+      })
+        .catch(error => next(error))
     })
-})
+  //#endregion
+
+  //#region OTHER ROUTES
+    app.post('/api/notes', (request, response, next) => {
+      const body = request.body
+
+      if (!body.content) {
+        return response.status(400).json({
+          error: 'content missing',
+        })
+      }
+
+      const note = new Note({
+        content: body.content,
+        important: body.important || false,
+      })
+
+      note.save().then(savedNote => {
+        response.json(savedNote)
+      })
+      .catch(error => next(error))
+    })
+
+    app.delete('/api/notes/:id', (request, response, next) => {
+      Note.findByIdAndDelete(request.params.id)
+        .then(result => {
+          response.status(204).end()
+        })
+        .catch(error => next(error))
+    })
+
+    app.put('/api/notes/:id', (request, response, next) => {
+      const { content, important } = request.body
+
+      Note.findById(request.params.id)
+        .then(note => {
+          if (!note) {
+            return response.status(404).end()
+          }
+
+          note.content = content
+          note.important = important
+
+          return note.save().then((updatedNote) => {
+            response.json(updatedNote)
+          })
+        })
+        .catch((error) => {
+          return next(error)
+        })
+    })
+  //#endregion
+
 //#endregion
 
 //#region ERROR HANDLERS
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+      return response.status(400).json({ error: error.message })
+    }
+
+    next(error)
   }
 
-  next(error)
-}
-
-// this has to be the last loaded middleware, also all the routes should be registered before this!
-app.use(errorHandler)
+  // this has to be the last loaded middleware, also all the routes should be registered before this!
+  app.use(errorHandler)
 //#endregion
 
 const PORT = process.env.PORT || 3002
