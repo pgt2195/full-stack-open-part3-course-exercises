6082f32383044269c121a9d63a4d1a2cf3617999
Paul Grossetête <paulgrossetete.code@gmail.com>
2025-06-30 16:47:06 +0200

    sauvegarde avant de passer aux exercices 4.8 à 4.12

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app = require('./app') // the actual Express application
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-app.listen(config.PORT, () => {
+app.listen(config.PORT, '0.0.0.0', () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
